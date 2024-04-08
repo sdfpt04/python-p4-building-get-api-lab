9794462aaad64b587c9561a78380caa8c544da9f
kevin <kevin@novek.io>
2024-04-09 01:08:08 +0300

    feat;completed assignment

--- server/app.py
+++ server/app.py
@@ -20,19 +20,43 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+
+    bakeries = []
+    for bakarie in Bakery.query.all():
+        bakerie_dict = {
+            "name":bakarie.name
+
+        }
+        bakeries.append(bakerie_dict)
+
+    response = make_response(
+        jsonify(bakeries),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get_or_404(id) 
+    response = make_response(jsonify(bakery.to_dict()), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    response = make_response(jsonify([bg.to_dict() for bg in baked_goods]), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    response = make_response(jsonify(baked_good.to_dict()), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
